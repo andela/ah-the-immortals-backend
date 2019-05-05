50c8c90fd1eed288c677f1663e82c724b0091fd8
James Maruhi <jamesmaruhi@Jamess-MacBook-Pro.local>
2019-05-05 21:38:06 +0300

    165305757-Fix(Social Authentication): Fix Error Messages
    - Fix serializer error messages to be more descriptive
    
    [#165305757]

--- authors/apps/authentication/serializers.py
+++ authors/apps/authentication/serializers.py
@@ -214,17 +214,38 @@ class SocialAuthSerializer(serializers.Serializer):
 
     provider = serializers.CharField(
         max_length=30,
-        required=True
+        allow_blank=True
     )
     access_token = serializers.CharField(
         max_length=255,
-        required=True
+        allow_blank=True
     )
     access_token_secret = serializers.CharField(
         max_length=255,
         allow_blank=True,
         default=""
     )
+
+    def validate(self, data):
+        """Method to validate provider and access token"""
+        provider = data.get('provider', None)
+        access_token = data.get('access_token', None)
+        access_token_secret = data.get('access_token_secret', None)
+        if not provider:
+            raise serializers.ValidationError(
+                'A provider is required for Social Login'
+            )
+
+        if not access_token:
+            raise serializers.ValidationError(
+                'An access token is required for Social Login'
+            )
+
+        if provider == 'twitter' and not access_token_secret:
+            raise serializers.ValidationError(
+                'An access token secret is required for Twitter Login'
+            )
+        return data
 
 
 class PasswordResetSerializer(serializers.Serializer):
