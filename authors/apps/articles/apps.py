df886723dbbc8da9967ca09c59e3d14ad66c0cde
Philip M Otieno <tmitchellb007@gmail.com>
2019-05-07 09:34:22 +0300

    165305769-ft(Search functionality): Article custom search and filtering
    -Picked commits
    -Add default filter backends in settings
    -Add a custom filter class specifying filter parameters
    -Add tests
    
    [Finishes  #165305769]

--- authors/apps/articles/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class ArticlesConfig(AppConfig):
-    name = 'articles'
-
-    def ready(self):
-        from authors.utils import notification_handlers
--- authors/apps/articles/urls.py
+++ authors/apps/articles/urls.py
@@ -6,6 +6,8 @@ from .views import (CommentAPIView, CommentDetailAPIView, FavoritesView,
                     ListUserFavoriteArticlesView, RateArticleAPIView,
                     RetrieveUpdateArticleAPIView)
 
+# from .filters import FilterArticlesView
+
 app_name = 'articles'
 
 urlpatterns = [
@@ -17,7 +19,6 @@ urlpatterns = [
          CommentDetailAPIView.as_view(), name='commentdetail'),
     path('articles/<slug>/', RetrieveUpdateArticleAPIView.as_view(),
          name='articles'),
-    path('tags/', FetchTags.as_view(), name="all_tags"),
     path('articles/<str:slug>/<str:vote_type>/vote/',
          LikeDislikeView.as_view(), name='likes'),
     path('articles/<str:slug>/favorite/',
@@ -26,8 +27,4 @@ urlpatterns = [
          ListUserFavoriteArticlesView.as_view(), name='get_favorite'),
     path('articles/<slug>/rate/',
          RateArticleAPIView.as_view(), name='rating_articles'),
-    path('articles/<slug>/comments/',
-         CommentAPIView.as_view(), name='comment'),
-    path('articles/<slug>/comments/<id>/',
-         CommentDetailAPIView.as_view(), name='commentdetail')
 ]
