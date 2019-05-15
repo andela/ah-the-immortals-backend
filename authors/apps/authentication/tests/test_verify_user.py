9ac05a891719a1c58e571e4997c1da8d52ece07f
Ogutu-Brian <brian.ogutu@andela.com>
2019-05-15 18:59:02 +0300

    166028283-bg(Signup Verification):  - Use authentication token for verifying user  - Update mailing template for verifying user [Finishes #166028283]

--- authors/apps/authentication/tests/test_verify_user.py
+++ authors/apps/authentication/tests/test_verify_user.py
@@ -7,41 +7,44 @@ class TestUserVerification(BaseTest):
     This class handles the testing of User Verification
     """
 
-    def test_successfull_verification(self):
-        token = self.generate_jwt_token('pablo@escobar.com', 'Escobar')
-        url = self.verification_of_user + token
+    def setUp(self):
+        super().setUp()
+        self.user.is_verified = False
+        self.user.save()
 
+    def verify(self, token):
+        """
+        Verifies a user
+        """
+        url = self.verification_of_user + token
         response = self.client.get(url)
+        return response
+
+    def test_successfull_verification(self):
+        token = self.generate_auth_token().key
+        response = self.verify(token)
         self.assertEqual(response.data['message'],
                          "You have been verified successfully")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_invalid_verification_token(self):
-        self.signup_user('Elchapo', 'el@chapo.com', 'Maina9176')
-        self.verify_user('el@chapo.com')
-        token = self.generate_jwt_token('el@chapo.com', 'Elchapo')
-        url = self.verification_of_user + token
-
-        response = self.client.get(url)
-
+    def test_used_verification_token(self):
+        token = self.generate_auth_token().key
+        self.verify(token)
+        response = self.verify(token)
         self.assertEqual(response.data['message'],
                          "Your email is already verified")
         self.assertEqual(response.status_code, status.HTTP_406_NOT_ACCEPTABLE)
 
     def test_invalid_verification_token(self):
-        token = "sidvfldifhsv"
-        url = self.verification_of_user + token
-
-        response = self.client.get(url)
+        token = self.generate_fake_auth_token()
+        response = self.verify(token)
         self.assertEqual(
             response.data['detail'], "Invalid Token. The token provided cannot be decoded!")
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
-    def test_expired_jwt_token(self):
-        token = self.generate_expired_token()
-        url = self.verification_of_user + token
-
-        response = self.client.get(url)
+    def test_expired_auth_token(self):
+        token = self.generate_expired_auth_token().key
+        response = self.verify(token)
         self.assertEqual(
             response.data['detail'],
             'The token used has expired. Please authenticate again!')
